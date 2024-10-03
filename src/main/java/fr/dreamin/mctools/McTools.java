c440da428abbc2a256ef031d45071244642e5b04
Samuel De Oliveira <samuel.deoliveir1@gmail.com>
2024-10-03 17:48:28 +0200

    correction to push lib

--- src/main/java/fr/dreamin/mctools/McTools.java
+++ src/main/java/fr/dreamin/mctools/McTools.java
@@ -5,5 +5,8 @@ import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class McTools {
+
+    @Getter private static String VERSION = "0.1.0";
+
     @Getter @Setter private static JavaPlugin instance;
 }
